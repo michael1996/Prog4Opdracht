aa5181cb33a843466f3ccbd1346dccb14b372914
michael1996 <michael19_96@hotmail.nl>
2018-05-14 17:26:25 +0200

    Added: Update and Delete Function for Dorms

--- controllers/Dorm_controller.js
+++ controllers/Dorm_controller.js
@@ -34,7 +34,7 @@ module.exports = {
     },
     getDorms(req,res,next){
         const query = {
-            sql: 'SELECT * FROM  studentenhuis',
+            sql: 'SELECT * FROM view_studentenhuis',
             timeout: 2000
         };
     
@@ -55,7 +55,7 @@ module.exports = {
         if (id)
         {
             const query = {
-                sql: 'SELECT * FROM  studentenhuis WHERE ID = ?',
+                sql: 'SELECT * FROM  view_studentenhuis WHERE ID = ?',
                 values: id,
                 timeout: 2000
             };
@@ -72,6 +72,75 @@ module.exports = {
         else 
         {
             this.getDorms;
+        }
+    },
+    updateDorm(req,res,next)
+    {
+        const id = req.params.id ||'';
+        assert(req.body.naam, 'firstname must be provided')
+        assert(req.body.adres, 'lastname must be provided')
+        const name = req.body.naam
+        const adres = req.body.adres
+        if (id)
+        {
+            const query = {
+                sql: 'UPDATE studentenhuis SET Naam = ?, Adres = ? WHERE ID = ?',
+                values: [name,adres,id],
+                timeout: 2000
+            };
+            console.log('QUERY: ' + query.sql);
+        
+            db.query( query, (error, rows, fields) => {
+                    if (error) {
+                        res.status(500).json(error.toString())
+                    } else {
+                        res.status(200).json(rows)
+                    }
+                });
+        }
+        else 
+        {
+            //error handling als er geen id gegeven is 
+        }
+    },
+    deleteDorm(req,res,next)
+    {
+        const id = req.params.id ||'';
+        if (id)
+        {
+            const querydeelnemers = {
+                sql: 'DELETE FROM deelnemers WHERE StudentenhuisID = ?',
+                values: id,
+                timeout: 2000
+            };
+            console.log('QUERY: ' + querydeelnemers.sql);
+            db.query( querydeelnemers, (error, rows, fields) => {
+                });
+                const querymaaltijd = {
+                    sql: 'DELETE FROM maaltijd WHERE StudentenhuisID = ?',
+                    values: id,
+                    timeout: 2000
+                };
+                console.log('QUERY: ' + querymaaltijd.sql);
+                db.query( querymaaltijd, (error, rows, fields) => {
+                    });
+            const query = {
+                sql: 'DELETE FROM studentenhuis WHERE ID = ?',
+                values: id,
+                timeout: 2000
+            };
+            console.log('QUERY: ' + query.sql);
+            db.query( query, (error, rows, fields) => {
+                    if (error) {
+                        res.status(500).json(error.toString())
+                    } else {
+                        res.status(200).json(rows)
+                    }
+                });
+        }
+        else 
+        {
+            //error handling als er geen id gegeven is 
         }
     }
 }
