69fc27f9d092a0f2a3a3651dc74e374c2ac37876
michael1996 <michael19_96@hotmail.nl>
2018-05-15 11:52:17 +0200

    Update: User_controller error handler

--- controllers/User_controller.js
+++ controllers/User_controller.js
@@ -12,21 +12,46 @@ module.exports = {
         assert.equal(typeof(req.body.email), 'string', "Argument 'email' must be a string.");
         assert.equal(typeof(req.body.password), 'string', "Argument 'password' must be a string.");
 
-        const query = {
-            sql: 'INSERT INTO user(Voornaam, Achternaam,Email,Password) VALUES (?, ?,?,?)',
-            values: [user.firstname, user.lastname,user.email,user.password ],
-            timeout: 2000
-        };
-    
-        console.log('QUERY: ' + query.sql);
-    
-        db.query( query, (error, rows, fields) => {
-                if (error) {
-                    res.status(500).json(error.toString())
-                } else {
-                    res.status(200).json(rows)
-    
+
+            const checkquery = {
+                sql: 'SELECT * FROM user WHERE Email = ?',
+                values: user.email,
+                timeout: 2000
+            };
+            db.query(checkquery,(error,rows)=>{
+                if(rows.length>0)
+                {
+                    res.send({
+                        "code": 412,
+                        "message":"email is al ingebruik",
+                        "date": Date()
+                    });
                 }
+                else
+                {
+                    const query = {
+                        sql: 'INSERT INTO user(Voornaam, Achternaam,Email,Password) VALUES (?, ?,?,?)',
+                        values: [user.firstname, user.lastname,user.email,user.password ],
+                        timeout: 2000
+                    };
+                
+                    console.log('QUERY: ' + query.sql);
+                
+                    db.query( query, (error, rows, fields) => {
+                            if (error) {
+                                res.status(500).json(error.toString())
+                            } else {
+                                console.log(rows)
+                                res.send({
+                                    "code": 200,
+                                    "token" :auth.encodeToken(rows.insertID,user.email),
+                                    "email" : user.email,
+                                });
+                            }
+                        });
+                }
+
+                    
             });
     },
     LoginUser(req,res,next){
@@ -47,14 +72,16 @@ module.exports = {
             else{
                 if(rows.length =1){
                     res.send({
-                        "token" :auth.encodeToken(rows[0].ID,email),
-                        "email" : email,
+                        "code": 200,
+                        "token" :auth.encodeToken(rows[0].ID,rows[0].Email),
+                        "email" : rows[0].Email,
                     });
                   }
                   else{
                     res.send({
                       "code":412,
-                      "success":"Email or Password is incorrect"
+                      "Login Failed": "Een of meer properties in de request body ontbreken of zijn foutief",
+                      "date": Date()
                         });
                   }
                 }
