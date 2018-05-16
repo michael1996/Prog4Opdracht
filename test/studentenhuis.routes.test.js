0b60b284003b6c2eb804a361a176280afae16f63
michael1996 <michael19_96@hotmail.nl>
2018-05-16 15:33:28 +0200

    Update: Testcases

--- test/studentenhuis.routes.test.js
+++ test/studentenhuis.routes.test.js
@@ -24,8 +24,12 @@ describe('Studentenhuis API POST', () => {
     });
     it('should throw an error when using invalid JWT token', (done) => {
         chai.request(server)
-        .get('/api/studentenhuis')
+        .post('/api/studentenhuis')
         .set('X-Access-Token',fakeToken)
+        .send({
+            "email": "hansklaas9@server.nl",
+            "password":"secret"
+        })
 
         .end((err,res)=>{
             res.should.have.status(401)
@@ -80,99 +84,174 @@ describe('Studentenhuis API GET all', () => {
 
     });
     it('should throw an error when using invalid JWT token', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
+        chai.request(server)
+        .get('/api/studentenhuis')
+        .set('X-Access-Token',fakeToken)
+
+        .end((err,res)=>{
+            res.should.have.status(401)
+            done()
+        })
     })
 
     it('should return all studentenhuizen when using a valid token', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
+        chai.request(server)
+        .get('/api/studentenhuis')
+        .set('X-Access-Token',validToken)
+
+        .end((err,res)=>{
+            res.should.have.status(200)
+            done()
+        })
     })
 })
 
 describe('Studentenhuis API GET one', () => {
+    let validToken = null;
+    let fakeToken = 0;
+    before(function(){
+        chai.request(server)
+            .post('/api/login')
+            .send({
+                "email": "hansklaas9@server.nl",
+                "password":"secret"
+            })
+            .end((err,res)=>{
+                validToken = res.body.token
+            })
+
+    });
     it('should throw an error when using invalid JWT token', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
+        chai.request(server)
+        .get('/api/studentenhuis/2')
+        .set('X-Access-Token',validToken)
+
+        .end((err,res)=>{
+            res.should.have.status(401)
+            done()
+        })
     })
 
     it('should return the correct studentenhuis when using an existing huisId', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
+        chai.request(server)
+        .get('/api/studentenhuis/2')
+        .set('X-Access-Token',validToken)
+
+        .end((err,res)=>{
+            res.should.have.status(200)
+            done()
+        })
     })
 
     it('should return an error when using an non-existing huisId', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
+        chai.request(server)
+        .get('/api/studentenhuis/200')
+        .set('X-Access-Token',validToken)
+
+        .end((err,res)=>{
+            res.should.have.status(404)
+            done()
+        })
     })
 })
 
 describe('Studentenhuis API PUT', () => {
+    let validToken = null;
+    let fakeToken = 0;
+    before(function(){
+        chai.request(server)
+            .post('/api/login')
+            .send({
+                "email": "hansklaas9@server.nl",
+                "password":"secret"
+            })
+            .end((err,res)=>{
+                validToken = res.body.token
+            })
+
+    });
     it('should throw an error when using invalid JWT token', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
+        chai.request(server)
+        .put('/api/studentenhuis/2')
+        .set('X-Access-Token',fakeToken)
+        .send({
+            "naam":"ahahahahahah ",
+            "adres":"aahhahaha"
+        })
+
+        .end((err,res)=>{
+            res.should.have.status(401)
+            done()
+        })
     })
 
     it('should return a studentenhuis with ID when posting a valid object', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
+        chai.request(server)
+        .put('/api/studentenhuis/15')
+        .set('X-Access-Token',validToken)
+        .send({
+            "naam":"ahahahahahah ",
+            "adres":"aahhahaha"
+        })
+
+        .end((err,res)=>{
+            res.should.have.status(200)
+            done()
+        })
     })
 
     it('should throw an error when naam is missing', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
+        chai.request(server)
+        .put('/api/studentenhuis')
+        .set('X-Access-Token',validToken)
+        .send({
+            "adres":"aahhahaha"
+        })
+
+        .end((err,res)=>{
+            res.should.have.status(404)
+            done()
+        })
     })
 
     it('should throw an error when adres is missing', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
+        chai.request(server)
+        .put('/api/studentenhuis')
+        .set('X-Access-Token',validToken)
+        .send({
+            "naam":"aahhahaha"
+        })
+
+        .end((err,res)=>{
+            res.should.have.status(404)
+            done()
+        })
     })
 })
 
 describe('Studentenhuis API DELETE', () => {
+    let validToken = null;
+    let fakeToken = 0;
+    before(function(){
+        chai.request(server)
+            .post('/api/login')
+            .send({
+                "email": "hansklaas9@server.nl",
+                "password":"secret"
+            })
+            .end((err,res)=>{
+                validToken = res.body.token
+            })
+
+    });
     it('should throw an error when using invalid JWT token', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
-    })
+        chai.request(server)
+        .del('/api/studentenhuis/15')
+        .set('X-Access-Token',fakeToken)
 
-    it('should return a studentenhuis when posting a valid object', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
-    })
-
-    it('should throw an error when naam is missing', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
-    })
-
-    it('should throw an error when adres is missing', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
+        .end((err,res)=>{
+            res.should.have.status(401)
+            done()
+        })
     })
 })
